fe39544248647a10cb7636db6e7cff9a7b1b5212
filippob <filippo.biscarini@gmail.com>
2019-11-01 13:16:11 +0100

    step 5

--- 5.steps/2.step_imputation.sh
+++ 5.steps/2.step_imputation.sh
@@ -1,6 +1,6 @@
---------------------------------------
+#--------------------------------------
 ## imputation of missing genotypes  ##
---------------------------------------
+#--------------------------------------
 
 ## set paths to executables
 plink=/home/filippo/Downloads/plink
@@ -13,7 +13,7 @@ $plink --dog --file dogs_filtered --recode vcf --out dogs_filtered	## check the 
 $plink --dog --file dogs_filtered --recode vcf-iid --out dogs_filtered
 
 ## Beagle
-if [-n $beagle]; then
+if [ $beagle = beagle ]; then
 	beagle gt=rice_filtered.vcf out=rice_imputed
 	beagle gt=dogs_filtered.vcf out=dogs_imputed
 else
@@ -28,3 +28,4 @@ beagle gt=dogs_filtered.vcf out=dogs_imputed
 $plink --vcf rice_imputed.vcf.gz --recode --out rice_imputed
 $plink --dog --vcf dogs_imputed.vcf.gz --recode --out dogs_imputed
 
+echo "DONE!"
